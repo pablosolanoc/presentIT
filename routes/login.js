37e54e35cb5fc00f3c16e1aaa91991eeb2de8ac1
Pablo Solano <pablosolano61098@gmail.com>
2022-01-31 01:28:25 -0500

    Trying new deployment commands

--- routes/login.js
+++ routes/login.js
@@ -119,10 +119,6 @@ router.get('/info', function(req, res){
     } 
 });
 
-router.get('/jaja', function(req, res){
-    res.send('HOla men como estas');
-});
-
 router.get('/logout', function(req, res){
     req.session.destroy();
     res.redirect(`${process.env.FRONT_END_ROUTE}`);
